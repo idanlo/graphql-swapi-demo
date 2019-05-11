a63837a30ab48e86441ed1d0a731f56699968860
idanlo <idan2110@gmail.com>
2019-05-11 15:47:52 +0300

    show all characters and their films in a grid

--- src/modules/characters/Characters.jsx
+++ src/modules/characters/Characters.jsx
@@ -1,10 +1,48 @@
 import React from 'react';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+import { Row, Col, Card, Divider } from 'antd';
+
+const GET_ALL_CHARACTERS = gql`
+  query {
+    allPersons {
+      name
+      id
+      films {
+        title
+      }
+    }
+  }
+`;
 
 function Characters() {
   return (
-    <div>
-      <h1>charaters</h1>
-    </div>
+    <Row gutter={16}>
+      <Query query={GET_ALL_CHARACTERS}>
+        {({ data, loading, error }) => {
+          if (loading) return <h1>Loading...</h1>;
+          if (error) return <h1>Error!</h1>;
+          if (data) {
+            console.log(data);
+            return data.allPersons.map(char => (
+              <Col key={char.id} xs={24} md={12} lg={8} xl={4}>
+                <Card
+                  title={char.name}
+                  style={{ height: 225, margin: '5px 0' }}
+                >
+                  <span>
+                    <Divider style={{ margin: '2px 0' }}>
+                      <b>Movies</b>
+                    </Divider>
+                    {char.films.map(film => film.title).join(', ')}
+                  </span>
+                </Card>
+              </Col>
+            ));
+          }
+        }}
+      </Query>
+    </Row>
   );
 }
 
