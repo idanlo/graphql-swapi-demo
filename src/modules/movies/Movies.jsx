ce8decbb9eea0ca7380b3cdd5f345cfe9d6f1b28
idanlo <idan2110@gmail.com>
2019-05-11 15:18:12 +0300

    display simple list of all films using graphql

--- src/modules/movies/Movies.jsx
+++ src/modules/movies/Movies.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+
+const GET_ALL_FILMS = gql`
+  query {
+    allFilms(orderBy: releaseDate_ASC) {
+      title
+      id
+      releaseDate
+    }
+  }
+`;
 
 function Movies() {
   return (
     <div>
-      <h1>movies</h1>
+      <Query query={GET_ALL_FILMS}>
+        {({ data, loading, error }) => {
+          if (loading) return <h1>Loading...</h1>;
+          if (error) return <h1>Error!</h1>;
+
+          if (data) {
+            console.log(data);
+            return data.allFilms.map(film => (
+              <h1 key={film.title}>{film.title}</h1>
+            ));
+          }
+        }}
+      </Query>
     </div>
   );
 }
