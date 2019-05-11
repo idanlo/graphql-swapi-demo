33d9cbbf72d61d36218d895cc942632e8ca697cb
idanlo <idan2110@gmail.com>
2019-05-11 15:07:46 +0300

    config react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { ApolloProvider } from 'react-apollo';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Create connection to the graphql server
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: 'https://swapi.graph.cool/'
+});
+
+const client = new ApolloClient({
+  cache,
+  link
+});
+
+const app = (
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
