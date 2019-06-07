57f5e53d075d003e820bb5307b1be7f8425170ff
idanlo <idan2110@gmail.com>
2019-06-07 18:25:28 +0300

    modify the movies component to display data like the characters component

--- src/modules/movies/Movies.jsx
+++ src/modules/movies/Movies.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import gql from 'graphql-tag';
 import Query from '../../components/Query';
+import { Card, Col, Pagination } from 'antd';
 
 const GET_ALL_FILMS = gql`
   query {
-    allFilms(orderBy: releaseDate_ASC) {
-      title
-      id
-      releaseDate
+    allFilms {
+      pageInfo {
+        endCursor
+        hasNextPage
+      }
+      totalCount
+      edges {
+        node {
+          title
+          id
+          releaseDate
+        }
+      }
     }
   }
 `;
@@ -17,9 +27,19 @@ function Movies() {
     <div>
       <Query
         query={GET_ALL_FILMS}
-        render={data =>
-          data.allFilms.map(film => <h1 key={film.title}>{film.title}</h1>)
-        }
+        render={data => (
+          <>
+            {data.allFilms.edges.map(film => (
+              <Col key={film.node.id} xs={24} md={12} lg={8} xl={4}>
+                <Card
+                  title={film.node.title}
+                  style={{ minHeight: 250, margin: '5px 0' }}
+                />
+              </Col>
+            ))}
+            <Pagination defaultCurrent={1} total={data.allFilms.totalCount} />
+          </>
+        )}
       />
     </div>
   );
