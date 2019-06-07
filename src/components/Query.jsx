b4634092b26efa1c50190a50aaaac7b58af4b894
idanlo <idan2110@gmail.com>
2019-06-07 18:20:46 +0300

    render error message and loading spinner on the Query component

--- src/components/Query.jsx
+++ src/components/Query.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import { Query as ApolloQuery } from 'react-apollo';
+import { Alert, Spin, Icon } from 'antd';
 
 function Query(props) {
   return (
     <ApolloQuery query={props.query}>
       {({ data, loading, error }) => {
-        if (loading) return <h1>Loading...</h1>;
-        if (error) return <h1>Error!</h1>;
+        if (loading)
+          return (
+            <div style={{ textAlign: 'center' }}>
+              <Spin
+                indicator={
+                  <Icon type="loading" style={{ fontSize: 48 }} spin />
+                }
+              />
+            </div>
+          );
+        if (error)
+          return (
+            <Alert
+              type="error"
+              showIcon
+              message={`There has been an error - ${error}`}
+            />
+          );
         if (data) {
           return props.render(data);
         }
